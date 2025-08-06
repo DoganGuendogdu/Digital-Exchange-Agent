9ae04775b14703300e2ca7b2648d0be964042572
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-06 11:40:44 +0200

    [Add] initialize Spring Boot Project version 4.0.0 utilizing JDK 24.

---
+++ src/test/java/com/digital_exchange_agent/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_exchange_agent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
