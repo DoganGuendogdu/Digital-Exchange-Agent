3bcbe28cb0d4474b40068011fc5b570dcf5ef401
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-08 16:25:11 +0200

    [Add] Account Controller.

---
+++ src/main/java/com/digital_exchange_agent/backend/controller/AccountController.java
@@ -0,0 +1,59 @@
+package com.digital_exchange_agent.backend.controller;
+
+import com.digital_exchange_agent.backend.dto.AccountDTO;
+import com.digital_exchange_agent.backend.entity.Account;
+import com.digital_exchange_agent.backend.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/account")
+@Slf4j
+public class AccountController {
+    private final AccountService accountService;
+    private final static Logger logger = LoggerFactory.getLogger(AccountController.class);
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@RequestBody @Validated AccountDTO accountDTO) {
+        logger.debug("Inserting Account into db");
+        var account = new Account(
+                accountDTO.name(),
+                accountDTO.surname(),
+                accountDTO.sex(),
+                accountDTO.email(),
+                accountDTO.password(),
+                accountDTO.phoneNumber());
+        logger.debug("Created Account '{}' from DTO: '{}'", account, accountDTO);
+
+        try {
+            logger.debug("Inserting Account into db.");
+            var createdAccount = accountService.createAccount(account);
+            var responseSuccess = new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
+            logger.debug("Success. Returning '{}'.", responseSuccess);
+            return responseSuccess;
+        } catch (Exception e) {
+            ResponseEntity<Account> responseFailure = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            logger.debug("Failed to save Account to database: {}", responseFailure);
+            return responseFailure;
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccounts(){
+        return new ResponseEntity<>(accountService.getAccounts(), HttpStatus.OK);
+//        return accountService.getAccounts();
+    }
+}
