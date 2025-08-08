6bbff499dea70922424fe146fc43b5af3d5fca1b
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-08 18:19:53 +0200

    [Delete] unused import statements.

--- src/main/java/com/digital_exchange_agent/backend/controller/AccountController.java
+++ src/main/java/com/digital_exchange_agent/backend/controller/AccountController.java
@@ -12,10 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-import static org.springframework.http.HttpStatus.CREATED;
-
 @RestController
 @RequestMapping("/account")
 @Slf4j
