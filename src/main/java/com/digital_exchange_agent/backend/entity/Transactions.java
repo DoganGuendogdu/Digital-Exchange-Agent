8ea8ff7808245c4dcbeb8df65c90b14e1c766327
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-13 15:35:52 +0200

    [Add] Transactions entity.

---
+++ src/main/java/com/digital_exchange_agent/backend/entity/Transactions.java
@@ -0,0 +1,156 @@
+package com.digital_exchange_agent.backend.entity;
+
+import com.digital_exchange_agent.backend.utility.PriorityLevel;
+import com.digital_exchange_agent.backend.utility.Status;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "TRANSACTIONS")
+public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, unique = true)
+    private int id;
+
+    @JoinColumn(name = "RECIPIENT_ID", nullable = false)
+    @ManyToOne
+    private Recipient recipient;
+
+    @JoinColumn(name = "ACCOUNT_ID", nullable = false)
+    @ManyToOne
+    private Account account;
+
+    @OneToMany
+    private List<Task> task;
+
+    @Column(name = "TOPIC", nullable = false)
+    private String topic;
+
+    @Column(name = "RESPONSE")
+    private String response;
+
+    @Column(name = "CUSTOM_INFO")
+    private String custom_info;
+
+    @Column(name = "TAGS")
+    private String tags;
+
+    @Column(name = "PRIORITY_LEVEL", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PriorityLevel priorityLevel;
+
+    @Column(name = "STATUS", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+        /*
+        TODO: Add time fields:
+        - EXPIRED_AT,
+        - RESPONDED_AT,
+        - CREATED_AT,
+     */
+
+    public Transactions(
+            Recipient recipient,
+            Account account,
+            String topic,
+            String response,
+            String custom_info,
+            String tags,
+            PriorityLevel priorityLevel,
+            Status status) {
+        this.recipient = recipient;
+        this.account = account;
+        this.topic = topic;
+        this.response = response;
+        this.custom_info = custom_info;
+        this.tags = tags;
+        this.priorityLevel = priorityLevel;
+        this.status = status;
+    }
+
+    public Transactions() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Recipient getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Recipient recipient) {
+        this.recipient = recipient;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Task> getTask() {
+        return task;
+    }
+
+    public void setTask(List<Task> task) {
+        this.task = task;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getCustomInfo() {
+        return custom_info;
+    }
+
+    public void setCustomInfo(String custom_info) {
+        this.custom_info = custom_info;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public PriorityLevel getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(PriorityLevel priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
