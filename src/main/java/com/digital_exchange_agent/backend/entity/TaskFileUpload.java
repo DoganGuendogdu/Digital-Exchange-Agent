2329e6575a9bde91ff4f6ad3c8be75f4c1731895
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-14 14:26:01 +0200

    [Add] TASK_FILE_UPLOAD entity while db relationship to TASK being enforced using inheritance.

---
+++ src/main/java/com/digital_exchange_agent/backend/entity/TaskFileUpload.java
@@ -0,0 +1,98 @@
+package com.digital_exchange_agent.backend.entity;
+
+import com.digital_exchange_agent.backend.utility.FileType;
+import com.digital_exchange_agent.backend.utility.Status;
+import jakarta.persistence.*;
+
+@Entity
+@PrimaryKeyJoinColumn(name = "ID")
+public class TaskFileUpload extends Task {
+    @Column(name = "FILE", nullable = false)
+    @Lob
+    private byte[] file;
+
+    @Column(name = "FILE_TYPE", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FileType fileType;
+
+    @Column(name = "CUSTOM_INFO")
+    private String customInfo;
+
+    @Column(name = "STATUS", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "STEP_NUMBER", nullable = false)
+    private int stepNumber;
+
+    @Column(name = "FILE_NAME", nullable = false)
+    private String fileName;
+
+    public TaskFileUpload() {
+        super();
+    }
+
+    public TaskFileUpload(
+            byte[] file,
+            FileType fileType,
+            String customInfo,
+            Status status,
+            int stepNumber,
+            String fileName) {
+        super();
+        this.file = file;
+        this.fileType = fileType;
+        this.customInfo = customInfo;
+        this.status = status;
+        this.stepNumber = stepNumber;
+        this.fileName = fileName;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public FileType getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(FileType fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getCustomInfo() {
+        return customInfo;
+    }
+
+    public void setCustomInfo(String customInfo) {
+        this.customInfo = customInfo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
