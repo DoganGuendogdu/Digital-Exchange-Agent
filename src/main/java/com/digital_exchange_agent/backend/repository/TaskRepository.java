64efdc533381a010e4b29e575724a11708b5b91f
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-18 18:07:31 +0200

    [Add] Repository for Task to execute CRUD operation against db.

---
+++ src/main/java/com/digital_exchange_agent/backend/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.digital_exchange_agent.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.digital_exchange_agent.backend.entity.Task;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
