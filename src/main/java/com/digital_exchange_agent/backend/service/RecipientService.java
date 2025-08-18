866eb2610f7b6d09d9ad6b34c31df58b5234b4f6
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-08-18 17:55:59 +0200

    [Add] service method to query Recipient data for testing.

--- src/main/java/com/digital_exchange_agent/backend/service/RecipientService.java
+++ src/main/java/com/digital_exchange_agent/backend/service/RecipientService.java
@@ -4,6 +4,9 @@ import com.digital_exchange_agent.backend.entity.Recipient;
 import com.digital_exchange_agent.backend.repository.RecipientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class RecipientService {
@@ -16,5 +19,15 @@ public class RecipientService {
 
     public Recipient createRecipient(Recipient recipient) throws Exception {
         return recipientRepository.save(recipient);
+    }
+
+    public Optional<List<Recipient>> getRecipients() {
+        return Optional.of(recipientRepository.findAll());
+    }
+
+    public Optional<Recipient> getDefaultRecipientForMVP() {
+        final Integer recipientID = 1;
+
+        return recipientRepository.findById(recipientID);
     }
 }
